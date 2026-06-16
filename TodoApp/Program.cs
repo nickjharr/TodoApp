4f14a9e5f4fd84d7a382b6dcb4644ae52335ec80
Nick Harrington <nickjharrington@btinternet.com>
2026-06-16 22:00:39 +0100

    feat: add -l flag to list todos without entering TUI

--- TodoApp/Program.cs
+++ TodoApp/Program.cs
@@ -1,5 +1,30 @@
+using Spectre.Console;
 using TodoApp;
 
 var store = TodoStore.CreateDefault();
-var app = new App(store);
-app.Run();
+
+if (args.Length > 0 && args[0] == "-l")
+{
+    var todos = store.Load();
+    if (todos.Count == 0)
+    {
+        AnsiConsole.MarkupLine("[grey]No todos.[/]");
+    }
+    else
+    {
+        foreach (var todo in todos)
+        {
+            var escaped = Markup.Escape(todo.Text);
+            if (todo.Completed)
+                AnsiConsole.MarkupLine($"[grey strikethrough][[x]] {escaped}[/]");
+            else
+            {
+                var color = todo.Priority switch { 2 => "yellow", 3 => "red", _ => "white" };
+                AnsiConsole.MarkupLine($"[{color}][[ ]] {escaped}[/]");
+            }
+        }
+    }
+    return;
+}
+
+new App(store).Run();
